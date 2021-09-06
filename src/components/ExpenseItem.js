83e47a7ae2505eb3c73eb3fd545ca1c29e84f0d7
pradnya237 <pradnya237@solacetechnologies.co.in>
2021-09-06 12:09:59 +0530

    date formmating done

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 function ExpenseItem  (props) 
 {
-    const expenseDate=new Date(2021, 2, 28);
-    const expenseTitle="Car Insurance";
-    const expenseAmount=247.00;
+    const year=props.date.getFullYear();
+    const month=props.date.toLocaleString('en-US',{month:'long'});
+    const day=props.date.toLocaleString('en-US',{day:'2-digit'});
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <div>
+            <div>{day}</div>
+            <div>{month}</div>
+            <div>{year}</div>
+            </div>
             <div className="expense-item__description">
                 <h1>{props.title}</h1>
                  <div className="expense-item__price">${props.amount}</div>
