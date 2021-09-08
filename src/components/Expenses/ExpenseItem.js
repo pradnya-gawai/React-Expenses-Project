3b83756a15f76194df3323267047101505406d5d
pradnya237 <pradnya237@solacetechnologies.co.in>
2021-09-08 15:17:04 +0530

    title updated

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,16 +2,22 @@ import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Cards from '../UI/Cards';
 function ExpenseItem  (props) 
-{
+{        let title="updated"
+
+    const clickHnadler=()=>
+    
+    {
+        console.log(title);
+    }
     return (
         <Cards className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-            <h1>{props.title}</h1>
+            <h1>{title}</h1>
             <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={()=>console.log("myclick")}
-            >Click Me</button>
+            <button onClick={clickHnadler}
+            >Change title</button>
         </Cards>);
 }
 
