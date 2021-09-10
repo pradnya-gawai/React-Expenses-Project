a39596be29e925116754d0e08b86882bad156568
pradnya237 <pradnya237@solacetechnologies.co.in>
2021-09-10 11:39:58 +0530

    select the event vlaue

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './ExpenseForm.css'
 const ExpenseForm = () => {
+    const titleChangeHandler =(event)=>
+    {
+        console.log(event.target.value);
+    }
 
     return(
         <form>
             <div className='new-expense__controls'>
                 <div className='new-expense__controls'>
-                <lable>Title</lable>
-                <input type='text'/>
+                <label>Title</label>
+                <input type='text' onChange={titleChangeHandler}/>
                 </div>
                 <div className='new-expense__controls'>
-                <lable>Amount</lable>
+                <label>Amount</label>
                 <input type='number' min='0.01' step='0.01'/>
                 </div>
                 <div className='new-expense__controls'>
-                <lable>Date</lable>
+                <label>Date</label>
                 <input type='date' min='2019-1-1' max='2022-12-31'/>
                 </div>
             </div>
