18822420693fa279d136bea1e7ba1d4836fcf2de
pradnya237 <pradnya237@solacetechnologies.co.in>
2021-09-17 15:27:16 +0530

    rendering List

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -15,7 +15,7 @@ function ExpenseItem  (props)
         <Cards className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-            <h1>{title}</h1>
+            <h1>{props.title}</h1>
             <div className="expense-item__price">${props.amount}</div>
             </div>
             <button onClick={clickHnadler}
