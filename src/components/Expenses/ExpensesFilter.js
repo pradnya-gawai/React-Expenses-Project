4bd3c4583fbe1fb19c3989009e5c3d0d676fd146
pradnya237 <pradnya237@solacetechnologies.co.in>
2021-09-17 11:02:36 +0530

    onchange Event add

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './ExpensesFilter.css';
 
 const ExpensesFilter = () => {
+    const dropdownChangeHandler =(event)  =>{
+        console.log(event.target.value) };
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select>
+        <select onChange={dropdownChangeHandler}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
@@ -17,3 +19,4 @@ const ExpensesFilter = () => {
     </div>
   );
 };
+export default ExpensesFilter;
