bc1e12701d728443f2952c1b31de8885cea3b172
pradnya237 <pradnya237@solacetechnologies.co.in>
2021-09-17 11:40:53 +0530

    selected prop added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,15 +5,17 @@ import Cards from "../UI/Cards";
 import  ExpensesFilter from'./ExpensesFilter';
 
 function Expenses(props){
+    const [filteredYear,setFilteredYear]=useState("2020")
     const filterChangeHandler =(selectedYear)=>
     {
     //     console.log("Expenses.js");
     //     console.log(selectedYear);
+    setFilteredYear(selectedYear);
     }
     return(
     <div>
     <Cards className="expenses">
-    <ExpensesFilter onChangeFilter={filterChangeHandler}/>
+    <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
 
     <ExpenseItem 
     title={props.items[0].title} 
