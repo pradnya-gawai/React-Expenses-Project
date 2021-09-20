6daa2e76444a0c17b00f016656323f9bbd1f31ca
pradnya237 <pradnya237@solacetechnologies.co.in>
2021-09-20 12:58:21 +0530

    chat folder added

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import 'Chart.css'
+import ChartBar from './ChartBar'
+
+const Chart =props =>
+{
+    return(
+        <div className='chart'>
+            {props.dataPoints.map((dataPoint) =>
+            (
+                <ChartBar
+                key={dataPoint.lable}
+                value={dataPoint.value}
+                maxValue={null}
+                lable={dataPoint.lable}
+                />
+            ) )
+            }
+        </div>
+    )
+
+}
+export default Chart;
+
+ 
---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const ChartBar = () =>
+{
+
+}
+export default ChartBar;
