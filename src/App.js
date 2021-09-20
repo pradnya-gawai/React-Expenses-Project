5f13a0235398b5857011f2d249d413ff4a26070f
pradnya237 <pradnya237@solacetechnologies.co.in>
2021-09-20 15:54:12 +0530

    data change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NewExpense from './components/NewExpense/NewExpense';
 const DUMMY_EXPENSES = [
   {
     id: 'e1',
-    title: 'Toilet Paper',
+    title: 'News Paper',
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
